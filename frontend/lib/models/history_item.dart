a48a119af5511b082529029aabbf0ecd82a6cc73
Arjun Rana <rana.arjun52000@gmail.com>
2026-08-24 05:57:38 +0530

    feat: add persistent favorite metadata

--- frontend/lib/models/history_item.dart
+++ frontend/lib/models/history_item.dart
@@ -9,6 +9,7 @@ class HistoryItem {
   final String localPath;
   final DateTime downloadedAt;
   final int fileSize;
+  final bool favorite;
 
   const HistoryItem({
     required this.id,
@@ -19,33 +20,24 @@ class HistoryItem {
     required this.localPath,
     required this.downloadedAt,
     required this.fileSize,
+    this.favorite = false,
   });
 
-  factory HistoryItem.fromJson(Map<String, dynamic> json) {
-    return HistoryItem(
-      id: (json['id'] as String?) ?? '',
-      title: (json['title'] as String?) ?? 'Unknown song',
-      artist: (json['artist'] as String?) ?? 'Unknown artist',
-      thumbnail: (json['thumbnail'] as String?) ?? '',
-      filename: (json['filename'] as String?) ?? '',
-      localPath: (json['local_path'] as String?) ?? '',
-      downloadedAt: DateTime.tryParse((json['downloaded_at'] as String?) ?? '') ?? DateTime.now(),
-      fileSize: (json['file_size'] as int?) ?? 0,
-    );
-  }
+  factory HistoryItem.fromJson(Map<String, dynamic> json) => HistoryItem(
+        id: json['id'] as String? ?? '', title: json['title'] as String? ?? 'Unknown song',
+        artist: json['artist'] as String? ?? 'Unknown artist', thumbnail: json['thumbnail'] as String? ?? '',
+        filename: json['filename'] as String? ?? '', localPath: json['local_path'] as String? ?? '',
+        downloadedAt: DateTime.tryParse(json['downloaded_at'] as String? ?? '') ?? DateTime.now(),
+        fileSize: (json['file_size'] as num?)?.toInt() ?? 0, favorite: json['favorite'] == true,
+      );
 
-  Map<String, dynamic> toJson() => {
-        'id': id,
-        'title': title,
-        'artist': artist,
-        'thumbnail': thumbnail,
-        'filename': filename,
-        'local_path': localPath,
-        'downloaded_at': downloadedAt.toIso8601String(),
-        'file_size': fileSize,
-      };
+  Map<String, dynamic> toJson() => {'id': id, 'title': title, 'artist': artist, 'thumbnail': thumbnail,
+    'filename': filename, 'local_path': localPath, 'downloaded_at': downloadedAt.toIso8601String(),
+    'file_size': fileSize, 'favorite': favorite};
 
   String encode() => jsonEncode(toJson());
+  factory HistoryItem.decode(String raw) => HistoryItem.fromJson(jsonDecode(raw) as Map<String, dynamic>);
 
-  factory HistoryItem.decode(String rawJson) => HistoryItem.fromJson(jsonDecode(rawJson) as Map<String, dynamic>);
+  HistoryItem copyWith({bool? favorite}) => HistoryItem(id:id,title:title,artist:artist,thumbnail:thumbnail,filename:filename,
+    localPath:localPath,downloadedAt:downloadedAt,fileSize:fileSize,favorite:favorite ?? this.favorite);
 }
